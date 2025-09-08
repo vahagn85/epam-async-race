b86c19d3e71e128fc978fe1eb27076c80dae1073
vahagn85 <v.ulikhanyan@yandex.ru>
2025-09-08 14:21:01 +0200

    refactor: update Button classname, add disable attr

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -2,15 +2,17 @@ interface ButtonProps {
   name: string;
   onClick?: () => void;
   className?: string;
+  disabled?: boolean;
 }
 
 function Button(props: ButtonProps) {
-  const { name, onClick, className } = props;
+  const { name, onClick, disabled, className } = props;
 
   return (
     <button
       onClick={onClick}
-      className={`text-white px-4 py-2 border border-gray-300 rounded-md shadow-sm bg-blue-500 hover:bg-blue-600 cursor-pointer focus:outline-none ${className || ''}`}
+      disabled={disabled}
+      className={`text-white px-4 py-2 border border-gray-300 rounded-md shadow-sm bg-blue-500 hover:bg-blue-600 cursor-pointer focus:outline-none ${className || ''} disabled:opacity-50 disabled:cursor-not-allowed`}
     >
       {name}
     </button>
