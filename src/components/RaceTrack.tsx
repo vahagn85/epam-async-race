d29da87eb1b4f910fd88af4eb08f566023fc623a
vahagn85 <v.ulikhanyan@yandex.ru>
2025-09-13 16:18:23 +0200

    refactor: update race track, show msg when no cars

--- src/components/RaceTrack.tsx
+++ src/components/RaceTrack.tsx
@@ -4,6 +4,14 @@ import TrackRow from './TrackRow';
 function RaceTrack() {
   const cars = useAppStore((state) => state.cars);
 
+  if (!cars.length) {
+    return (
+      <div className="w-full bg-gray-800 flex justify-center min-h-20 items-center mt-8">
+        <p className="text-gray-200">No Cars. Please generate or create car</p>
+      </div>
+    );
+  }
+
   return (
     <div className="w-full bg-gray-800 flex flex-col mt-8">
       {cars.map((car) => (
